5c3cc6b9e356cff8864b58f3fa80cf9dcc7f55cf
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-05 12:02:05 +0530

    age increment and decrement button

---
+++ BodyMassIndexApp-iOS/View/HomeScreen/SelectAge.swift
@@ -0,0 +1,18 @@
+//
+//  SelectAge.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 05/05/24.
+//
+
+import SwiftUI
+
+struct SelectAge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectAge()
+}
