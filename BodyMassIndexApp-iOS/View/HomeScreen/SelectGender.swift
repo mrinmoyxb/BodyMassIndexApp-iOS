31603dcb6538b14b0e70b63da7eb2c4f5ae481d4
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-05 11:12:43 +0530

    gender card

---
+++ BodyMassIndexApp-iOS/View/HomeScreen/SelectGender.swift
@@ -0,0 +1,18 @@
+//
+//  SelectGender.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 05/05/24.
+//
+
+import SwiftUI
+
+struct SelectGender: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectGender()
+}
