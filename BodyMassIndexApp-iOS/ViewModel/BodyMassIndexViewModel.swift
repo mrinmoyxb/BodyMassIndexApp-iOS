1adf8ea901f84f16b3492793135a12431c6a749b
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-05 19:50:31 +0530

    ObservableObject in swift UI

--- BodyMassIndexApp-iOS/ViewModel/BodyMassIndexViewModel.swift
+++ BodyMassIndexApp-iOS/ViewModel/BodyMassIndexViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class BodyMassIndexViewModel: ObservableObject{
+    
+    // 1: male, 0: female
+    @Published var sex: Int = -1
+    
+    // parameters for BMI
+    @Published var age: Int = -1
+    @Published var weight: Int = -1
+    @Published var height: Int = -1
+    
+    // result
+    @Published var result: Int = 0
+    
+    // function to calculate BMI
+    func calculateBodyMassIndex(){
+        result = weight/(height*height)
+    }
+}
