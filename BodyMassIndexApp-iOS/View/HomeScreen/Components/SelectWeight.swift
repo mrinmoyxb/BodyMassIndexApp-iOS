07766edeacd14ed4bd8211de8c37905af66aa1a9
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-05 12:32:19 +0530

    Weight Bar

---
+++ BodyMassIndexApp-iOS/View/HomeScreen/Components/SelectWeight.swift
@@ -0,0 +1,18 @@
+//
+//  SelectWeight.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 05/05/24.
+//
+
+import SwiftUI
+
+struct SelectWeight: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectWeight()
+}
