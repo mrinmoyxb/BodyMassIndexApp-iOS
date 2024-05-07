a5955544f1daf93322d5d1ec8d0edd382eb0b83b
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-07 17:20:57 +0530

    fixed navigation and function called bug

---
+++ BodyMassIndexApp-iOS/ViewModel/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 06/05/24.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
--- BodyMassIndexApp-iOS/ViewModel/testModel.swift
+++ BodyMassIndexApp-iOS/ViewModel/testModel.swift
@@ -2,16 +2,7 @@
 //  testModel.swift
 //  BodyMassIndexApp-iOS
 //
-//  Created by Mrinmoy Borah on 05/05/24.
+//  Created by Mrinmoy Borah on 06/05/24.
 //
 
 import Foundation
-
-class testModel: ObservableObject{
-    @Published var age: Int = -1
-    @Published var result: Int = 0
-    
-    func demotest(){
-        result = age*100
-    }
-}
