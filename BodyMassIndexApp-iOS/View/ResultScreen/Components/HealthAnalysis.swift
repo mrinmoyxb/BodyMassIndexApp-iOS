d976ade2cc70b68b9bd6d2cb874592f189bace76
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-08 10:33:28 +0530

    updated viewmodel with BMI function and risk predict function

---
+++ BodyMassIndexApp-iOS/View/ResultScreen/Components/HealthAnalysis.swift
@@ -0,0 +1,18 @@
+//
+//  HealthAnalysis.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 08/05/24.
+//
+
+import SwiftUI
+
+struct HealthAnalysis: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HealthAnalysis()
+}
