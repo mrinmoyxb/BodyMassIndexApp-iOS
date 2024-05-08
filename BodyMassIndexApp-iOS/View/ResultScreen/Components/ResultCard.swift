8b64515e201c0c65282a43ae3cad5a24b0777556
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-08 09:43:33 +0530

    result card UI

---
+++ BodyMassIndexApp-iOS/View/ResultScreen/Components/ResultCard.swift
@@ -0,0 +1,18 @@
+//
+//  ResultCard.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 08/05/24.
+//
+
+import SwiftUI
+
+struct ResultCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultCard()
+}
