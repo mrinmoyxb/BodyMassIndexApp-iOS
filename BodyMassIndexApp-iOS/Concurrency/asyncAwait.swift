d75bc00d2ae35dc2ac8a20363dabdd137adca1c3
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-09-02 19:54:04 +0530

    async await implementation

--- BodyMassIndexApp-iOS/Concurrency/asyncAwait.swift
+++ BodyMassIndexApp-iOS/Concurrency/asyncAwait.swift
@@ -13,18 +13,30 @@ struct UserData{
     let following: Int
 }
 
+
 class AsyncAwaitViewModel: ObservableObject{
     @Published var userInfo: UserData?
     
     
-// This method will suspend all other functions while executing the first function so total execution time will be of 6s, we can use Task{} to solve the problem but async let is more preferable
-    /* func fetchData() async {
+/* This method will suspend all other functions while executing the first function so total execution time will be of 6s, we can use Task{} to solve the problem but "async let" is more preferable
+    
+ func fetchData() async {
         let posts = await fetchPosts()
         let followers = await fetchFollowers()
         let following = await fetchFollowing()
 
         self.userInfo = UserData(posts: posts, followers: followers, following: following)
-    }*/
+ }
+*/
+    
+// as here we have not used await in front of each function it will not block or suspend the second function
+    func fetchData() async{
+        async let posts = fetchPosts()
+        async let followers = fetchFollowers()
+        async let following = fetchFollowing()
+        
+        self.userInfo = await UserData(posts: posts, followers: followers, following: following)
+    }
     
     func fetchPosts() async -> Int {
         try? await Task.sleep(nanoseconds: 3_000_000_000) // 3s
@@ -33,32 +45,41 @@ class AsyncAwaitViewModel: ObservableObject{
     
     func fetchFollowers() async -> Int {
         try? await Task.sleep(nanoseconds: 2_000_000_000) // 2s
-        return 9
+        return 300
     }
     
     func fetchFollowing() async -> Int {
         try? await Task.sleep(nanoseconds: 1_000_000_000) // 1s
-        return 9
+        return 120
     }
 }
 
+
 struct ConcurrnentView: View{
+    
+    @StateObject var viewModel = AsyncAwaitViewModel()
     var body: some View{
-        HStack{
-            VStack{
-                Text("2")
-                Text("Posts")
-            }.frame(width: 100)
-            VStack{
-                Text("200")
-                Text("Followers")
-            }.frame(width: 100)
-            VStack{
-                Text("2")
-                Text("Following")
-            }.frame(width: 100)
-            
-        }
+            HStack{
+                if let stats = viewModel.userInfo{
+                VStack{
+                    Text("\(stats.posts)")
+                    Text("Posts")
+                }.frame(width: 100)
+                    
+                VStack{
+                    Text("\(stats.followers)")
+                    Text("Followers")
+                }.frame(width: 100)
+                    
+                VStack{
+                    Text("\(stats.following)")
+                    Text("Following")
+                }.frame(width: 100)
+                
+            }else{
+                ProgressView()
+            }
+        }.task{await viewModel.fetchData()}
     }
 }
 
--- BodyMassIndexApp-iOS/Concurrency/asyncAwait1.swift
+++ BodyMassIndexApp-iOS/Concurrency/asyncAwait1.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+struct User: Identifiable{
+    let id = UUID().uuidString
+    let name: String
+    let email: String
+}
+
+class AsyncAwait1: ObservableObject{
+    
+    @Published var allUsers:[User]? = [User]()
+    
+    init(){
+        Task{
+            await fetchUsers()
+        }
+    }
+    
+    func fetchUsers() async {
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        
+        let users: [User] = [
+            .init(name: "alex", email: "alex@mail.com"),
+            .init(name: "bob", email: "bob@mail.com"),
+            .init(name: "cam", email: "cam@mail.com")
+        ]
+        self.allUsers = users
+    }
+}
+
 struct asyncAwait1: View {
+    
+    @StateObject var viewModel = AsyncAwait1()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+                if let allUsers = viewModel.allUsers{
+                    List{
+                        ForEach(allUsers){user in
+                            VStack(alignment: .leading){
+                                Text(user.name)
+                                Text(user.email)
+                            }
+                        }
+                }
+            }else{
+                ProgressView()
+            }
+        }
     }
 }
 
