18114652b038efb58eb859037b8b02bbf21049f2
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-09-02 19:53:52 +0530

    async await implementation

---
+++ BodyMassIndexApp-iOS/Concurrency/asyncAwait1.swift
@@ -0,0 +1,18 @@
+//
+//  asyncAwait1.swift
+//  BodyMassIndexApp-iOS
+//
+//  Created by Mrinmoy Borah on 02/09/24.
+//
+
+import SwiftUI
+
+struct asyncAwait1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    asyncAwait1()
+}
